60045551f37c5318e03d7f1fcd8af8002d1d1307
MarekPouchly <marekpouchl8@gmail.com>
2023-02-19 17:51:40 +0100

    Create Basic Logic To Add Card To Grid

--- app.js
+++ app.js
@@ -14,13 +14,11 @@ class Book {
     }
 }
 
-function addBookToLibrary() {
-  // do stuff here
-}
-
 const openModalButton = document.querySelector('[data-modal-target]');
 const overlay = document.getElementById('overlay');
-const modal = document.querySelector(openModalButton.dataset.modalTarget);
+const modal = document.getElementById('modal');
+const addBookForm = document.getElementById('addBookForm');
+const books = document.querySelector('.books');
 
 openModalButton.addEventListener('click', () => {
     openModal(modal);
@@ -30,12 +28,61 @@ overlay.addEventListener('click', () => {
     closeModal(modal);
 })
 
-function openModal(modal) {
+function openModal() {
     modal.classList.add('active');
     overlay.classList.add('active');
 }
 
-function closeModal(modal) {
+function closeModal() {
     modal.classList.remove('active');
     overlay.classList.remove('active');
+}
+
+addBookForm.onsubmit = addBookToLibrary;
+
+function addBookToLibrary() {
+    const newBook = getBookFromForm();
+    saveBookToLibrary(newBook);
+    updateBooksGrid()
+    closeModal()
+}
+
+function getBookFromForm() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const isRead = document.getElementById('isRead').value;
+    return new Book(title, author, pages, isRead);
+}
+
+function saveBookToLibrary(newBook) {
+    myLibrary.push(newBook);
+}
+
+function updateBooksGrid() {
+    resetBooksGrid();
+    for (const book of myLibrary) {
+        createBookCard(book)
+    }
+}
+
+function createBookCard(book) {
+    const card = `
+        <div class="card">
+            <p>${book.title}</p>
+            <p>${book.author}</p>
+            <p>${book.pages}</p>
+            <button class="btn">${isBookRead(book.isRead)}</button>
+            <button class="btn">Remove</button>
+        </div>
+    `;
+    books.innerHTML += card;
+}
+
+function isBookRead(isRead) {
+    return isRead === true ? 'Read' : 'Not Read';
+}
+
+function resetBooksGrid() {
+    books.innerHTML = ''
 }
