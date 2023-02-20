0e399506842ac836cec6e86f5c617bf65c9bbf32
MarekPouchly <marekpouchl8@gmail.com>
2023-02-20 20:25:36 +0100

    Rework Book card creation with toggle read and remove func

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ addBookForm.onsubmit = addBookToLibrary;
 function addBookToLibrary() {
     const newBook = getBookFromForm();
     saveBookToLibrary(newBook);
-    updateBooksGrid()
-    closeModal()
+    updateBooksGrid();
+    closeModal();
 }
 
 function getBookFromForm() {
@@ -67,24 +67,58 @@ function updateBooksGrid() {
 }
 
 function createBookCard(book) {
-    const card = `
-        <div class="card">
-            <p>${book.title}</p>
-            <p>${book.author}</p>
-            <p>${book.pages}</p>
-            <div class="buttons">
-                <button class="btn">${isBookRead(book.isRead)}</button>
-                <button class="btn">Remove</button>
-            </div>
-        </div>
-    `;
-    books.innerHTML += card;
-}
+    const bookCard = document.createElement('div');
+    const titleOfBook = document.createElement('p');
+    const authorOfBook = document.createElement('p');
+    const bookPages = document.createElement('p');
+    const cardButtons = document.createElement('div');
+    const readBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
 
-function isBookRead(isRead) {
-    return isRead === true ? 'Read' : 'Not Read';
+    bookCard.classList.add('card');
+    cardButtons.classList.add('buttons');
+    readBtn.classList.add('btn');
+    removeBtn.classList.add('btn');
+
+    readBtn.onclick = toggleIsRead;
+    removeBtn.onclick = removeBook;
+
+    titleOfBook.textContent = `${book.title}`;
+    authorOfBook.textContent = `${book.author}`;
+    bookPages.textContent = `${book.pages}`;
+    removeBtn.textContent = 'Remove';
+
+    if (book.isRead) {
+        readBtn.textContent = 'Read';
+        readBtn.classList.add('read-btn')
+    } else {
+        readBtn.textContent = 'Not Read';
+        readBtn.classList.add('not-read-btn')
+    }
+
+    bookCard.appendChild(titleOfBook);
+    bookCard.appendChild(authorOfBook);
+    bookCard.appendChild(bookPages);
+    cardButtons.appendChild(readBtn);
+    cardButtons.appendChild(removeBtn);
+    bookCard.appendChild(cardButtons);
+    books.appendChild(bookCard);
 }
 
 function resetBooksGrid() {
     books.innerHTML = ''
 }
+
+function toggleIsRead(e) {
+    const title = e.target.parentNode.parentNode.firstChild.textContent;
+    const book = myLibrary.find(book => book.title === title);
+    book.isRead = !book.isRead;
+    updateBooksGrid();
+}
+
+function removeBook(e) {
+    const title = e.target.parentNode.parentNode.firstChild.textContent;
+    myLibrary = myLibrary.filter( book => book.title !== title )
+    updateBooksGrid();
+}
+
